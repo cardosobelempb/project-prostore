ca8dd45a8ca46cfc594327c7de537afd6ed35258
claudiobelempb <claudio.c.lima@hoymail.com>
2025-04-07 14:13:45 -0300

    10. Root Layout & Constants

--- web/app/(root)/page.tsx
+++ web/app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "Home",
+}
+
 const HomePage = () => {
 	return (
 		<>
