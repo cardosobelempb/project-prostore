221b0e9dbd7d94acbbce4f908d1f147b07cbed42
claudiobelempb <claudio.c.lima@hoymail.com>
2025-04-07 14:06:49 -0300

    first commit

---
+++ web/app/(root)/page.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+	return (
+		<>
+			<h1>Prostore</h1>
+		</>
+	)
+}
+export default HomePage
---
+++ web/app/assets/index.ts
@@ -0,0 +1,37 @@
+import loader from "./loader.gif"
+import banner01 from "./images/banner-1.jpg"
+import banner02 from "./images/banner-2.jpg"
+import logo from "./images/logo.svg"
+import promo from "./images/promo.jpg"
+import p11 from "./images/sample-products/p1-1.jpg"
+import p12 from "./images/sample-products/p1-2.jpg"
+import p21 from "./images/sample-products/p2-1.jpg"
+import p22 from "./images/sample-products/p2-2.jpg"
+import p31 from "./images/sample-products/p3-1.jpg"
+import p32 from "./images/sample-products/p3-2.jpg"
+import p41 from "./images/sample-products/p4-1.jpg"
+import p42 from "./images/sample-products/p4-2.jpg"
+import p51 from "./images/sample-products/p5-1.jpg"
+import p52 from "./images/sample-products/p5-2.jpg"
+import p61 from "./images/sample-products/p6-1.jpg"
+import p62 from "./images/sample-products/p6-2.jpg"
+
+export {
+	banner01,
+	banner02,
+	logo,
+	promo,
+	loader,
+	p11,
+	p12,
+	p21,
+	p22,
+	p31,
+	p32,
+	p41,
+	p42,
+	p51,
+	p52,
+	p61,
+	p62,
+}
