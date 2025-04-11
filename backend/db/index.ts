7e06edb498f8f61f61a64f436453ab4529db1bfa
claudiobelempb <claudio.c.lima@hoymail.com>
2025-04-11 07:49:25 -0300

    21. Seed Sample Data

---
+++ backend/db/index.ts
@@ -0,0 +1,14 @@
+import p11 from './sample-products/p1-1.jpg';
+import p12 from './sample-products/p1-2.jpg';
+import p21 from './sample-products/p2-1.jpg';
+import p22 from './sample-products/p2-2.jpg';
+import p31 from './sample-products/p3-1.jpg';
+import p32 from './sample-products/p3-2.jpg';
+import p41 from './sample-products/p4-1.jpg';
+import p42 from './sample-products/p4-2.jpg';
+import p51 from './sample-products/p5-1.jpg';
+import p52 from './sample-products/p5-2.jpg';
+import p61 from './sample-products/p6-1.jpg';
+import p62 from './sample-products/p6-2.jpg';
+
+export { p11, p12, p21, p22, p31, p32, p41, p42, p51, p52, p61, p62 };
